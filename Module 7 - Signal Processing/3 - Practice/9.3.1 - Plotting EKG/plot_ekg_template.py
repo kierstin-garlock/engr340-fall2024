847fc3cd9b20032b5ebbe8a7c548fe9456663ea8
kierstin-garlock <garlockr@dukes.jmu.edu>
2024-10-29 12:54:23 -0400

    9.3.1 ekg data done

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -8,11 +8,21 @@ path = '../../../data/ekg/mitdb_201.csv'
 # load data in matrix from CSV file; skip first two rows
 
 ### Your code here ###
+data = np.loadtxt(path, delimiter=",", skiprows=2)
 
 # save each vector as own variable
 
 ### Your code here ###
+V1 = data[:,0]
+V2 = data[:,1]
+V3 = data[:,2]
 
 # use matplot lib to generate a single
 
-### Your code here ###
+### Your code here ##
+plt.title("ekg data signal")
+plt.xlabel("Time (s)")
+plt.ylabel("mV")
+plt.plot(V1, V2)
+plt.plot(V1, V3)
+plt.show()
